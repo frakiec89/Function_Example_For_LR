b6940fabf41a8e47f8551cbd227ef60d575c2a36
Frakiec <Frakiec@DESKTOP-1TIO821>
2024-08-29 10:15:43 +0400

    Добавил нахождение   пересечения функции   с осью  X

--- Function_Example_For_LR/Program.cs
+++ Function_Example_For_LR/Program.cs
@@ -42,24 +42,27 @@ void Myfunction(double xMin, double xMax, double xStep)
         return;
     }
 
-
     if (xStep < 0)
     {
         PrintRed("Шаг меньше нуля");
         return;
     }
 
-
     PrintYellow("_____Ответ____");
     /// переберем функцию 
     for (double i = xMin; i <= xMax; i += xStep)
     {
-        double y = -Math.Pow(i, 2) - 2; // формула
-
+        double y = GetValueFunction(i);        
         PrintYellow($"x={i}: y={y}"); // Ответ 
     }
+    PrintYellow($"Пересечение функции с осью X В точке: {GetValueFunction(0)}");
 }
 
+
+double GetValueFunction (double x )
+{
+    return -1 * Math.Pow(x, 2) - 2; // формула
+}
 void PrintGreen(string message)
 {
     ConsoleColor color = Console.ForegroundColor; // запомнит текущий цвет
@@ -75,8 +78,6 @@ void PrintYellow(string message)
     Console.WriteLine(message);  //  выведем  сообщение 
     Console.ForegroundColor = color; // вернем  базовый цвет
 }
-
-
 
 
 void PrintRed(string message)
